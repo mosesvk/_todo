f704c11131d0b67fab78cd385f09a727b3feb8a1
mosesvk <kaumatule93@gmail.com>
2023-07-26 14:29:46 -0600

    udpate

---
+++ .history/client/src/components/ListItem_20230524143153.jsx
@@ -0,0 +1,46 @@
+import {useState} from 'react'
+import TickIcon from './TickIcon'
+import axios from 'axios'
+import ProgressBar from 'react-bootstrap/ProgressBar'
+import Modal from './Modal'
+
+const ListItem = ({task, getData}) => {
+  const {date, id, progress, title, user_email:email} = task
+
+  const [showModal, setShowModal] = useState(false)
+
+  const deleteData = async (e) => {
+
+    try {
+      axios.delete(`${import.meta.env.VITE_SERVER_URL}/${task.id}`)
+
+      alert(`Delete Successful - todo id: ${id}`)
+      setShowModal(false)
+      getData()
+
+    } catch (err) {
+      console.error(err)
+    }
+  } 
+
+  const percent = progress > 80 ? 'success' : progress > 50 ? 'warning' : 'danger'
+
+  return (
+    <div className='list-item'>
+      <div className='info-container'>
+        <TickIcon />
+        <p className='task-title'>{title}</p>
+        <ProgressBar variant={percent} now={percent} label={`${progress}%`}/>
+      </div>
+
+      <div className='button-container'>
+        <button className='edit' onClick={() => setShowModal(true)}>EDIT</button>
+        <button className='delete' onClick={deleteData}>DELETE</button>
+      </div>
+
+      {showModal && <Modal task={task} mode={'edit'} setShowModal={setShowModal} getData={getData}/>}
+    </div>
+  )
+}
+
+export default ListItem
